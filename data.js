4e3253ab645dd20cce75daf121556b650bbb22fe
Dtrusso8 <153931048+Dtrusso8@users.noreply.github.com>
2024-09-26 09:16:26 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-image-1",
+      "name": "Image (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 816,
+      "initialViewParameters": {
+        "yaw": -0.15229439110000698,
+        "pitch": 0.21272666288686182,
+        "fov": 1.3633411682890908
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.2135830122516147,
+          "pitch": -0.2177702129757222,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
